3349248503047843df200b7598476544a630d3e2
jinqiheng <jinqiheng@mini1.cn>
2020-08-14 10:45:43 +0800

    feat: 项目初始化

---
+++ Blog/webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development' ? true : false;
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'cheap-module-eval-source-map' : false,
+  devServer: {
+    contentBase: './dist'
+  },
+  externals: {
+    React: "react",
+    ReactDOM: "react-dom"
+  },
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: isDev ? true : false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'chin的个人空间'
+    })
+  ],
+  optimization: {
+    removeAvailableModules: isDev ? false : true,
+    removeEmptyChunks: isDev ? false : true,
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+};
