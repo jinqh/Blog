08afe5751c89109850f789786ab1cc6ea6752148
金其恒 <jinqiheng@C15920.local>
2021-08-17 16:46:28 +0800

    feat: 生产环境ip

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const fs = require('fs');
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const fs = require("fs");
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const port = 3000;
 
-app.use('/api', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }))
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://47.106.23.81:3001",
+    changeOrigin: true,
+  })
+);
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + "/dist"));
 
-app.get('*', (req, res) => {
-  if (fs.existsSync(__dirname + '/dist/index.html')) {
-    res.sendFile(__dirname + '/dist/index.html');
+app.get("*", (req, res) => {
+  if (fs.existsSync(__dirname + "/dist/index.html")) {
+    res.sendFile(__dirname + "/dist/index.html");
   } else {
-    res.send('路径错误或正在构建中');
+    res.send("路径错误或正在构建中");
   }
 });
 
 app.listen(port, () => {
-  console.log(`Blog listening at http://localhost:${port}`);
+  console.log(`Blog listening at http://47.106.23.81:${port}`);
 });
